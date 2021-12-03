6396ff5e6e515fea14b6cef9335a2f17cb90a00e
tarszhou <vem@qq.com>
2021-12-03 11:09:12 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <open-data type="userAvatarUrl"></open-data>
+  <open-data type="userNickName"></open-data>
+  <view class="aaaa">1</view>
+  <view class="aaaa">2</view>
+  <view class="aaaa">3</view>
+</template>
